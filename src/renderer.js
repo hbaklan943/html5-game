219944cf0c3a0cd5445c5a7e45d7b0adcd076720
hbaklan943 <hbaklan943@gmail.com>
2023-08-14 21:51:06 +0300

    fixed not passing below, changed jumping behavior

--- src/renderer.js
+++ src/renderer.js
@@ -4,6 +4,7 @@ const KeyboardHelper = { left: 37, up: 38, right: 39, down: 40, space: 32 };
 let rightPressed = false;
 let leftPressed = false;
 let upPressed = false;
+let upHolding = false;
 let downPressed = false;
 let spacePressed = false;
 let spaceHolding = false;
@@ -22,6 +23,7 @@ function keyDownHandler(event) {
   }
   if (event.keyCode === KeyboardHelper.down) {
     downPressed = true;
+    characterBody.setAwake(true);
   }
 }
 function keyUpHandler(event) {
@@ -37,6 +39,7 @@ function keyUpHandler(event) {
   }
   if (event.keyCode === KeyboardHelper.up) {
     upPressed = false;
+    upHolding = false;
   }
   if (event.keyCode === KeyboardHelper.down) {
     downPressed = false;
@@ -78,7 +81,7 @@ let dynamicBox = planck.Box(0.5, 0.5);
 let fixtureDef = {
   shape: dynamicBox,
   density: 1.0,
-  friction: 0.6,
+  friction: 0.9,
 };
 let characterFix = characterBody.createFixture(fixtureDef);
 
@@ -86,37 +89,39 @@ let timeStep = 1 / 60;
 let velocityIterations = 8;
 let positionIterations = 3;
 
+world.on("pre-solve", function (contact, oldManifold) {
+  if (downPressed) {
+    contact.setEnabled(false);
+  }
+  //console.log("Contact!");
+  let fixA = contact.getFixtureA();
+  let fixB = contact.getFixtureB();
+
+  let isCharPlatformContact;
+  let contactingPlatform;
+  if (fixA.m_body.m_type == "static") {
+    isCharPlatformContact = true;
+    contactingPlatform = fixA;
+  }
+  if (fixB.m_body.m_type == "static") {
+    isCharPlatformContact = true;
+    contactingPlatform = fixB;
+  }
+  if (!isCharPlatformContact) {
+    return;
+  }
+
+  let p = characterBody.getPosition();
+
+  if (p.y <= contactingPlatform.m_body.c_position.c.y + 1) {
+    contact.setEnabled(false);
+  }
+});
+
 // This function will be called on each frame update (tick)
 function gameLoop(delta) {
   // Step the physics simulation
   stats.begin();
-
-  world.on("pre-solve", function (contact, oldManifold) {
-    let fixA = contact.getFixtureA();
-    let fixB = contact.getFixtureB();
-
-    let isCharPlatformContact;
-    let contactingPlatform;
-    if (fixA.m_body.m_type == "static") {
-      isCharPlatformContact = true;
-      contactingPlatform = fixA;
-    }
-    if (fixB.m_body.m_type == "static") {
-      isCharPlatformContact = true;
-      contactingPlatform = fixB;
-    }
-    if (!isCharPlatformContact) {
-      return;
-    }
-
-    let p = characterBody.getPosition();
-
-    if (p.y < contactingPlatform.m_body.c_position.c.y + 1) {
-      contact.setEnabled(false);
-    } else if (downPressed) {
-      contact.setEnabled(false);
-    }
-  });
 
   world.step(timeStep * delta, velocityIterations, positionIterations);
   //console.log(characterBody.getLinearVelocity());
@@ -127,10 +132,12 @@ function gameLoop(delta) {
     //console.log("pressing right");
     characterBody.applyForce(planck.Vec2(-70, 0), planck.Vec2(0, 0));
   }
-  if (spacePressed && !spaceHolding) {
-    characterBody.applyLinearImpulse(planck.Vec2(0, 7), planck.Vec2(0, 0));
+  if (upPressed && !upHolding) {
+    let vel = characterBody.getLinearVelocity();
+    vel.y = 5;
+    characterBody.setLinearVelocity(vel);
     //console.log("applied impulse");
-    spaceHolding = true;
+    upHolding = true;
   }
 
   // Update the sprite position based on the physics body position
